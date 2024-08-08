2188572251ab5864d5ee027db0dd91302921b753
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2024-08-08 11:51:10 +0100

    Started character details section

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,18 @@ public class Main {
         System.out.println("You will see a side of Dublin never seen before!");
         System.out.println("Except by people who live there I guess...");
         System.out.println("Anyway, would you like to start a New Game?");
-        System.out.println("Yes/No");
+        System.out.println("y/n");
+        String keyboard = scanner.nextLine();
+        if (keyboard.equals("y")) {
+            System.out.println("Excellent. Lets get you started traveller!");
+        } else {
+            System.out.println("Oh. Well piss off then");
+        }
 
-
+        // Character details
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name);
 
     }
 }
