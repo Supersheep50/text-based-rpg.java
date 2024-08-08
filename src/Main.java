3ffdd99688f79ebacafcd286ac5f601252a287f1
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2024-08-08 11:33:18 +0100

    Created title with ASCI and intro

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-    System.out.println("\n" +
-            "███████ ██   ██  █████  ██████   ██████  ██     ██ ███████      ██████  ███████     ████████ ██   ██ ███████     ██      ██ ███████ ███████ ███████ ██    ██ \n" +
-            "██      ██   ██ ██   ██ ██   ██ ██    ██ ██     ██ ██          ██    ██ ██             ██    ██   ██ ██          ██      ██ ██      ██      ██       ██  ██  \n" +
-            "███████ ███████ ███████ ██   ██ ██    ██ ██  █  ██ ███████     ██    ██ █████          ██    ███████ █████       ██      ██ █████   █████   █████     ████   \n" +
-            "     ██ ██   ██ ██   ██ ██   ██ ██    ██ ██ ███ ██      ██     ██    ██ ██             ██    ██   ██ ██          ██      ██ ██      ██      ██         ██    \n" +
-            "███████ ██   ██ ██   ██ ██████   ██████   ███ ███  ███████      ██████  ██             ██    ██   ██ ███████     ███████ ██ ██      ██      ███████    ██    \n" +
-            "                                                                                                                                                             \n" +
-            "                                                                                                                                                             \n");
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        // Game title
+        System.out.println("\n" +
+                "███████ ██   ██  █████  ██████   ██████  ██     ██ ███████      ██████  ███████     ████████ ██   ██ ███████     ██      ██ ███████ ███████ ███████ ██    ██ \n" +
+                "██      ██   ██ ██   ██ ██   ██ ██    ██ ██     ██ ██          ██    ██ ██             ██    ██   ██ ██          ██      ██ ██      ██      ██       ██  ██  \n" +
+                "███████ ███████ ███████ ██   ██ ██    ██ ██  █  ██ ███████     ██    ██ █████          ██    ███████ █████       ██      ██ █████   █████   █████     ████   \n" +
+                "     ██ ██   ██ ██   ██ ██   ██ ██    ██ ██ ███ ██      ██     ██    ██ ██             ██    ██   ██ ██          ██      ██ ██      ██      ██         ██    \n" +
+                "███████ ██   ██ ██   ██ ██████   ██████   ███ ███  ███████      ██████  ██             ██    ██   ██ ███████     ███████ ██ ██      ██      ███████    ██    \n" +
+                "                                                                                                                                                             \n" +
+                "                                                                                                                                                             \n");
+
+
+        // Game Intro
+        System.out.println("Welcome to the Shadows of the Liffey videogame!");
+        System.out.println("Prepare to explore the depths of Dublin in this text based RPG");
+        System.out.println("You will see a side of Dublin never seen before!");
+        System.out.println("Except by people who live there I guess...");
+        System.out.println("Anyway, would you like to start a New Game?");
+        System.out.println("Yes/No");
+
+
+
+    }
 }
 
